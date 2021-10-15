374637d454021db00232c03e985ebef703c8be27
Jon Cram <webignition@gmail.com>
2021-10-15 09:53:58 +0100

    Update Parser/TypeParser

--- src/Parser/TypeParser.php
+++ src/Parser/TypeParser.php
@@ -3,11 +3,12 @@
 namespace webignition\InternetMediaType\Parser;
 
 use webignition\StringParser\StringParser;
+use webignition\StringParser\UnknownStateException;
 
 /**
  * Parses out the type from an internet media type string.
  */
-class TypeParser extends StringParser
+class TypeParser
 {
     public const TYPE_SUBTYPE_SEPARATOR = '/';
     public const STATE_IN_TYPE = 1;
@@ -25,57 +26,51 @@ class TypeParser extends StringParser
         '\\'
     ];
 
-    /**
-     * @throws TypeParserException
-     */
-    public function parse(string $inputString): string
+    private StringParser $stringParser;
+
+    public function __construct()
     {
-        return parent::parse(trim($inputString));
-    }
-
-    /**
-     * @throws TypeParserException
-     */
-    protected function parseCurrentCharacter(): void
-    {
-        switch ($this->getCurrentState()) {
-            case self::STATE_UNKNOWN:
-                $this->setCurrentState(self::STATE_IN_TYPE);
-
-                break;
-
-            case self::STATE_IN_TYPE:
-                if ($this->isCurrentCharacterInvalid()) {
-                    $this->setCurrentState(self::STATE_INVALID_INTERNAL_CHARACTER);
-                } elseif ($this->isCurrentCharacterTypeSubtypeSeparator()) {
-                    $this->setCurrentState(self::STATE_LEFT_TYPE);
-                } else {
-                    $this->appendOutputString();
-                    $this->incrementCurrentCharacterPointer();
-                }
-
-                break;
-
-            case self::STATE_LEFT_TYPE:
-                $this->stop();
-
-                break;
-
-            case self::STATE_INVALID_INTERNAL_CHARACTER:
+        $this->stringParser = new StringParser([
+            StringParser::STATE_UNKNOWN => function (StringParser $stringParser) {
+                $stringParser->setState(self::STATE_IN_TYPE);
+            },
+            self::STATE_IN_TYPE => function (StringParser $stringParser) {
+                $this->handleInTypeState($stringParser);
+            },
+            self::STATE_LEFT_TYPE => function (StringParser $stringParser) {
+                $stringParser->stop();
+            },
+            self::STATE_INVALID_INTERNAL_CHARACTER => function (StringParser $stringParser) {
                 throw new TypeParserException(
-                    'Invalid internal character after at position ' . $this->getCurrentCharacterPointer(),
+                    'Invalid internal character after at position ' . $stringParser->getPointer(),
                     1
                 );
+            },
+        ]);
+    }
+
+    /**
+     * @throws TypeParserException
+     * @throws UnknownStateException
+     */
+    public function parse(string $input): string
+    {
+        return $this->stringParser->parse(trim($input));
+    }
+
+    private function handleInTypeState(StringParser $stringParser): void
+    {
+        $character = $stringParser->getCurrentCharacter();
+        $isCharacterInvalid = in_array($character, $this->invalidCharacters);
+        $isCharacterTypeSubtypeSeparator = self::TYPE_SUBTYPE_SEPARATOR === $character;
+
+        if ($isCharacterInvalid || $isCharacterTypeSubtypeSeparator) {
+            $stringParser->setState(
+                $isCharacterInvalid ? self::STATE_INVALID_INTERNAL_CHARACTER : self::STATE_LEFT_TYPE
+            );
+        } else {
+            $stringParser->appendOutputString();
+            $stringParser->incrementPointer();
         }
-    }
-
-    private function isCurrentCharacterInvalid(): bool
-    {
-        return in_array($this->getCurrentCharacter(), $this->invalidCharacters);
-    }
-
-    private function isCurrentCharacterTypeSubtypeSeparator(): bool
-    {
-        return self::TYPE_SUBTYPE_SEPARATOR == $this->getCurrentCharacter();
     }
 }
